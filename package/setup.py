6ec7d19fa9c102c9bd8d02fff50ee39584d06f74
Karen Leung <karen.ym.leung@gmail.com>
2025-03-30 18:18:07 -0700

    changed package list

--- package/setup.py
+++ package/setup.py
@@ -3,12 +3,11 @@ from glob import glob
 from setuptools import setup
 
 setup(
-    name="dmol-book",
-    version="1.3.2",
-    description="Style and Imports for dmol Book",
-    author="Andrew D White",
-    author_email="andrew.white@rochester.edu",
-    url="https://dmol.pub",
+    name="lmc-book",
+    version="0.0.1",
+    description="Style and Imports for lmc book",
+    author="Karen LEung",
+    author_email="kymleung@uw.edu",
     license="MIT",
     packages=["dmol"],
     install_requires=[
@@ -18,35 +17,14 @@ setup(
         "ipython!=8.7.0",
         "jax",
         "jaxlib",
-        "pandas",
-        "tensorflow>=2.7",
-        "seaborn",
-        "tabulate",
-        "scikit-learn",
-        "pydot",
         "pillow>=8.3.2",
-        "mordred[full]",
-        "moviepy",
-        "networkx",
-        "MDAnalysis",
-        "emlp==1.0.2",
-        "tensorflow-probability",
-        "dm-haiku",
-        "selfies",
-        "tensorflowjs",
-        "exmol",
-        "rdkit>=2022",
-        "sympy",
-        "e3nn",
-        "torch==1.12",  # note - if you want to change this, change the github action too
-        "simpletransformers==0.63.9",
         "linkify-it-py",
     ],
     test_suite="tests",
     long_description="""
-# Style and Imports for dmol Book
+# Style and Imports for lmc Book
 
-This is the style and imports for deep learning for molecules and materials
+This is based on the style and imports for deep learning for molecules and materials
 written by Andrew White. Please see the [dmol book](https://dmol.pub) or
 root package at [github](https://github.com/whitead/dmol-book).
 """,
@@ -55,7 +33,7 @@ root package at [github](https://github.com/whitead/dmol-book).
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Controls",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Typing :: Typed",
     ],
